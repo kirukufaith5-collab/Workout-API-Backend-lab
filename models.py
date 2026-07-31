367b6d3531fd931ec4df1edf451c8383a13deaec
kirukufaith5-collab <kirukufaith5@gmail.com>
2026-07-31 15:39:13 +0300

    Create routes.py file

--- models.py
+++ models.py
@@ -3,3 +3,13 @@ from sqlalchemy.orm import validates
 from datetime import date
 
 db =SQLAlchemy()
+
+class Exercise(db.Model):
+    __tablename__='exercises'
+
+    id = db.Column(db.Integer ,primary_key=True)
+    name =db.Column(db.String ,nullable = False)
+    category= db.column(db.String ,nullable=False)
+    equipment_needed =db.Column(db.Boolean,default=False,nullable=False)
+
+    workout_exerices =db.relationship('WorkoutExercise',back_populates='exercise')
